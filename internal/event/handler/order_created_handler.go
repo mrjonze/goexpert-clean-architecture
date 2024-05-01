ed7f8613e14e82b8c7428d80c904c10a305a578f
Luis Correia Lima Neto <mr.jonze@gmail.com>
2024-05-01 17:24:08 -0300

    copiando arquivos e gerando módulo com dependências

---
+++ internal/event/handler/order_created_handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mrjonze/goexpert-clean-architecture/pkg/events"
+	"sync"
+)
+
+type OrderCreatedHandler struct {
+	RabbitMQChannel *amqp091.Channel
+}
+
+func NewOrderCreatedHandler(rabbitMQChannel *amqp091.Channel) *OrderCreatedHandler {
+	return &OrderCreatedHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	}
+}
+
+func (h *OrderCreatedHandler) Handle(event events.EventInterface, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Order created: %v", event.GetPayload())
+	jsonOutput, _ := json.Marshal(event.GetPayload())
+
+	msgRabbitMq := amqp091.Publishing{
+		ContentType: "application/json",
+		Body:        jsonOutput,
+	}
+
+	h.RabbitMQChannel.Publish("amq.direct",
+		"",
+		false,
+		false,
+		msgRabbitMq,
+	)
+}
